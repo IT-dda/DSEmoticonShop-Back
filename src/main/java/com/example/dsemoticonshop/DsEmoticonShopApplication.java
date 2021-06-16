ba1fc4fd56b86cf746a0a857993d30d22561ae79
jeong-yeji <jeongyeji.mail@gmail.com>
2021-06-16 22:51:25 +0900

    초기 설정

---
+++ src/main/java/com/example/dsemoticonshop/DsEmoticonShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsemoticonshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEmoticonShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsEmoticonShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dsemoticonshop/DsEmoticonShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsemoticonshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEmoticonShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
