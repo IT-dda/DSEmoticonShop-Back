b98c609e9c652560328af89e8301310db4e36e82
jeong-yeji <jeongyeji.mail@gmail.com>
2021-06-28 05:38:55 +0900

    DAO conflict 최종..

---
+++ src/main/java/com/example/dsemoticonshop/repository/GroupEmoticonRepository.java
@@ -0,0 +1,7 @@
+package com.example.dsemoticonshop.repository;
+
+import com.example.dsemoticonshop.entity.GroupEmoticon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupEmoticonRepository extends JpaRepository<GroupEmoticon, Integer> {
+}
